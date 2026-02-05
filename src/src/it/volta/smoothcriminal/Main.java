dc6c0c1e8eb9117b3962b5dd5ce6d01469905209
Lotan Teny <tenylot.s@iisvoltapescara.edu.it>
2026-02-05 22:00:43 +0100

    Fatto il Labirinto con la matrice di char, ismuro, isuscita, ancora da migliorare tanto

--- src/src/it/volta/smoothcriminal/Main.java
+++ src/src/it/volta/smoothcriminal/Main.java
@@ -1,4 +1,11 @@
 package it.volta.smoothcriminal;
 
+import it.volta.smoothcriminal.model.Labirinto;
+
 public class Main {
+    public static void main(String[] args) {
+        Labirinto labirinto = new Labirinto();
+
+        System.out.println(labirinto.isMuro(10,10));
+    }
 }
--- src/src/it/volta/smoothcriminal/model/Labirinto.java
+++ src/src/it/volta/smoothcriminal/model/Labirinto.java
@@ -1,66 +1,57 @@
 package it.volta.smoothcriminal.model;
 
 public class Labirinto {
-    String rawMaze =
-            "████████████████████████████\n" +
-                    "   █  █  █                 █  █\n" +
-                    "█  █  █  █  ████  █  ███████  █\n" +
-                    "█           █  █  █           █\n" +
-                    "███████  ████  █  █  █  ████  █\n" +
-                    "█           █     █  █         \n" +
-                    " █  █\n" +
-                    "████  █  ████  ████  ██████████\n" +
-                    "█     █  █     █  █     █     █\n" +
-                    "████  ███████  █  ██████████  █\n" +
-                    "█     █     █        █        █\n" +
-                    "█  ███████  █  ███████  ████  █\n" +
-                    "█  █        █           \n" +
-                    "   █  █\n" +
-                    "████  █  ███████  ████  ███████\n" +
-                    "█  █  █  █        █           █\n" +
-                    "█  ████  ████████████████  ████\n" +
-                    "█  █     █                    █\n" +
-                    "█  ████  ████  █  ███████  ████\n" +
-                    "█  █     █     █     █  \n" +
-                    "      █\n" +
-                    "█  ████  █  ███████  ████  ████\n" +
-                    "█              █     █        \n" +
-                    "████████████████████████████";
+    char mat[][];
+    int righe, colonne;
+    public String labirinto = "   ████████████████████████████\n" +
+            "                              █\n" +
+            "█  ██████████  ██████████  ████\n" +
+            "█           █  █     █        █\n" +
+            "████  █████████████  ████  ████\n" +
+            "█  █              █  █        █\n" +
+            "█  █  █  █  █  ████  ████  ████\n" +
+            "█     █  █  █  █  █  █     █  █\n" +
+            "███████  ███████  █  ████  █  █\n" +
+            "█              █        █  █  █\n" +
+            "████  █  ███████  █  ████  █  █\n" +
+            "█  █  █        █  █     █     █\n" +
+            "█  ███████  █  ███████  █  █  █\n" +
+            "█  █  █     █  █        █  █  █\n" +
+            "█  █  ████  ████  █  █  █  ████\n" +
+            "█  █  █        █  █  █     █  █\n" +
+            "█  █  █  █  ███████  █  █  █  █\n" +
+            "█        █  █        █  █     █\n" +
+            "█  ████████████████  █  █  ████\n" +
+            "█           █        █  █     \n" +
+            "████████████████████████████  U";
 
-    char[][] mazeMatrix = convertToCharMatrix(rawMaze);
 
-    for (int i=0; i<mazeMatrix.length(); i++) {
-        for (int j=0; j<mazeMatrix[0].length(); j++) {
-            System.out.print(mazeMatrix[i][j]);
-        }
-        System.out.println();
-    }
+    public Labirinto() {
+        String[] rows = labirinto.split("\n");
+        righe = rows.length;
+        colonne = rows[0].length();
+        mat = new char[righe][colonne];
 
-    public static char[][] convertToCharMatrix(String mazeStr) {
-        String[] lines = mazeStr.split("\n");
-        int rows = lines.length;
-        int cols = 0;
-
-        // Determine max width for the matrix [cite: 1, 2, 3, 4]
-        for (String line : lines) {
-            cols = Math.max(cols, line.length());
-        }
-
-        char[][] matrix = new char[rows][cols];
-
-        for (int i = 0; i < rows; i++) {
-            String currentLine = lines[i];
-            for (int j = 0; j < cols; j++) {
-                if (j < currentLine.length()) {
-                    // Use the original char or swap '█' for '#' for easier typing
-                    char c = currentLine.charAt(j);
-                    matrix[i][j] = (c == '█') ? '#' : '.';
-                } else {
-                    // Fill empty space if lines are uneven
-                    matrix[i][j] = '.';
-                }
+        for(int i = 0; i < righe; i++) {
+            for(int j = 0; j < rows[i].length(); j++) {
+                mat[i][j] = rows[i].charAt(j);
             }
         }
-        return matrix;
     }
+
+    public int getCellula(int x, int y) {
+        return 0;
+    }
+
+    public boolean isMuro(int x, int y) {
+        if (mat[x][y] == '█') return true;
+        return false;
+    }
+
+    public boolean isUscita(int x, int y) {
+        if (mat[x][y] == 'U') return true;
+        return false;
+    }
+
+
 }
